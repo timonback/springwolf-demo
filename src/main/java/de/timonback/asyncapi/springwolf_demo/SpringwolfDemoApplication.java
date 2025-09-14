95299b2d13318989864235db50991a6398a92795
Timon Back <timonback@users.noreply.github.com>
2025-09-14 15:58:53 +0200

    Spring Boot Initializr

---
+++ src/main/java/de/timonback/asyncapi/springwolf_demo/SpringwolfDemoApplication.java
@@ -0,0 +1,13 @@
+package de.timonback.asyncapi.springwolf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwolfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwolfDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/timonback/asyncapi/springwolf_demo/SpringwolfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.timonback.asyncapi.springwolf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwolfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
