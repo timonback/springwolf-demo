95b33851377e037c04644def918bfd742676028b
Timon Back <timonback@users.noreply.github.com>
2025-09-21 01:19:59 +0200

    document publisher

--- src/main/java/de/timonback/asyncapi/springwolf_demo/producer/AnotherProducer.java
+++ src/main/java/de/timonback/asyncapi/springwolf_demo/producer/AnotherProducer.java
@@ -2,6 +2,9 @@
 package de.timonback.asyncapi.springwolf_demo.producer;
 
 import de.timonback.asyncapi.springwolf_demo.dto.AnotherPayloadDto;
+import io.github.springwolf.bindings.kafka.annotations.KafkaAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
+import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,11 @@ public class AnotherProducer {
     @Autowired
     private KafkaTemplate<String, AnotherPayloadDto> kafkaTemplate;
 
+    @AsyncPublisher(operation = @AsyncOperation(
+            channelName = "another-topic",
+            description = "Sends another payload message"
+    ))
+    @KafkaAsyncOperationBinding
     public void sendMessage(AnotherPayloadDto msg) {
         kafkaTemplate.send("another-topic", msg);
     }
